d9060d7f6a1f2205790b816330e37fe9b9355366
LuisCoss <LuisFernando.Coss@bosch.com>
2021-10-31 16:38:59 -0600

    First Test Case

--- stats.c
+++ stats.c
@@ -1,12 +1,25 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
+    unsigned char index = 0;
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    s.max = numberset[0]; 
+    s.min = numberset[0];
+    for (index = 0 ; index < setlength; index ++){
+        s.average += numberset[index];
+        if (numberset[index] >= s.max) {
+            s.max = numberset[index];// Get Max Number 
+        } 
+        if (numberset[index] <= s.min){
+            s.min = numberset[index]; // Get Min Number
+        }  
+    }
 
-
+    s.average = s.average/setlength;// Average Calculation
 
     return s;
 }
