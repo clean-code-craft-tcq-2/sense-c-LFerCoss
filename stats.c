8b7f019feb76a86fdff376270c68f93e436077fd
LuisCoss <LuisFernando.Coss@bosch.com>
2021-10-31 16:13:56 -0600

    Working on firts test case only

--- stats.c
+++ stats.c
@@ -5,7 +5,11 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+
+
+
+    return s;
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+//int emailAlertCallCount = 0;
+//int ledAlertCallCount = 0;
