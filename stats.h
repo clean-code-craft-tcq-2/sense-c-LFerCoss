9d48d40b7a334faa2b85d3d113fd06024fa88b5f
LuisCoss <LuisFernando.Coss@bosch.com>
2021-10-31 17:48:04 -0600

    Test Case 3 in pogress

--- stats.h
+++ stats.h
@@ -1,12 +1,14 @@
 #include <math.h>
 
-#ifndef STATS
-    #define STATS
     struct Stats{
         float average;
         float min;
         float max;
     };
-#endif
 
 struct Stats compute_statistics(const float* numberset, int setlength);
+void emailAlerter();
+void ledAlerter();
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
