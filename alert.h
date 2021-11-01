1b5dc3bcdf634b3ae5ccaf2caede9f3a683ae06d
LuisCoss <LuisFernando.Coss@bosch.com>
2021-10-31 18:00:42 -0600

    Test Case 3 in pogress

--- alert.h
+++ alert.h
@@ -1,5 +1,8 @@
 #include "stats.h"
 
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
 typedef void (*alerter_funcptr)();
 void check_and_alert(const float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
